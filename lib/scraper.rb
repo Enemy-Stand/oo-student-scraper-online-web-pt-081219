9fd7b63fe68782baa1e9336357d7b0f749f8c67c
Joseph Sesti <sestijoey63@gmail.com>
2019-10-02 17:29:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,7 +33,7 @@ class Scraper
       end
     end
     
-    student[:profile_quote] = profile_page.css("profile_quote").text if profile_page.css(".profile_quote").text
+    student[:profile_quote] = profile_page.css(".profile_quote").text if profile_page.css(".profile_quote").text
     student[:bio] = profile_page.css("div.bio-content content-holder div.description-holder p").text if profile_page.css("div.bio-content content-holder div.description-holder p").text
     student
   end
